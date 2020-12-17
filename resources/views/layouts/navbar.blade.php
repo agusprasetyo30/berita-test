e71003a7efcdf8f56a1ce08504774147a28250de
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-12-17 21:54:03 +0700

    finish authentification

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,25 +6,39 @@
          <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-               <a class="nav-link {{ Request::is('admin/berita*') ? ' active' : '' }}" href="{{ route('admin.berita.index') }}">Berita</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link {{ Request::is('admin/kategori*') ? ' active' : '' }}" href="{{ route('admin.kategori.index') }}">Kategori</span></a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link {{ Request::is('admin/pengguna*') ? ' active' : '' }}" href="{{ route('admin.pengguna.index') }}">Pengguna</span></a>
-            </li>
-            <li class="nav-item dropdown ">
-               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Nama Pengguna
-               </a>
-               <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Logout</a>
-               </div>
-            </li>
-         </ul>
+         <!--Jika keadaan sudah login-->
+         @if (\Auth::user())
+            <ul class="navbar-nav ml-auto">
+               <li class="nav-item">
+                  <a class="nav-link {{ Request::is('admin/berita*') ? ' active' : '' }}" href="{{ route('admin.berita.index') }}">Berita</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link {{ Request::is('admin/kategori*') ? ' active' : '' }}" href="{{ route('admin.kategori.index') }}">Kategori</span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link {{ Request::is('admin/pengguna*') ? ' active' : '' }}" href="{{ route('admin.pengguna.index') }}">Pengguna</span></a>
+               </li>
+               <li class="nav-item dropdown ">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     {{ \Auth::user()->name ?? 'Nothing' }}
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                     <form action="{{ route('logout') }}" method="post" id="logout">
+                        @csrf
+                        <a class="dropdown-item" href="#" onclick="document.getElementById('logout').submit();">Logout</a>
+                     </form>
+                  </div>
+               </li>
+            </ul>
+
+         <!--Jika keadaan sudah belum/tidak login-->
+         @else
+            <ul class="navbar-nav ml-auto">
+               <li class="nav-item">
+                  <a class="nav-link active" href="{{ route('login') }}">Login</span></a>
+               </li>
+            </ul>
+         @endif
       </div>
    </div>
 </nav>
